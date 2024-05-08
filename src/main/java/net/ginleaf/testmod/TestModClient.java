3d7f8f2a9bc06405ed7bf37623ee93e0ede201d0
Ginleaff <BenitoiteTeeth@proton.me>
2024-05-08 16:22:55 -0400

    Initial commit

---
+++ src/main/java/net/ginleaf/testmod/TestModClient.java
@@ -0,0 +1,10 @@
+package net.ginleaf.testmod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class TestModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
