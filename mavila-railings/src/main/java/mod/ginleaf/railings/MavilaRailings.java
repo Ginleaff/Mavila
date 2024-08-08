cc325b534ae01c58b2cb679aa6c5174d793e47a7
Ginleaff <benitoiteteeth@proton.me>
2024-08-08 01:30:30 -0400

    Test Resources

--- mavila-railings/src/main/java/mod/ginleaf/railings/MavilaRailings.java
+++ mavila-railings/src/main/java/mod/ginleaf/railings/MavilaRailings.java
@@ -2,32 +2,34 @@ package mod.ginleaf.railings;
 
 import mod.ginleaf.railings.block.HorizontalBorderBlock;
 import net.fabricmc.api.ModInitializer;
-import net.minecraft.block.Block;
+import net.minecraft.block.AbstractBlock;
 import net.minecraft.block.MapColor;
 import net.minecraft.block.enums.NoteBlockInstrument;
 import net.minecraft.item.BlockItem;
 import net.minecraft.item.Item;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
+import net.minecraft.sound.BlockSoundGroup;
 import net.minecraft.util.Identifier;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class MavilaRailings implements ModInitializer {
 
-	public static final String MOD_ID = "mavila_quiver";
+	public static final String MOD_ID = "mavila_railings";
     public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
-	private static final Identifier RAILINGS_ID = Identifier.of(MOD_ID, "railings");
-	public static final HorizontalBorderBlock TEST_BLOCK = new HorizontalBorderBlock(Block.Settings.create()
+	private static final Identifier TEST_RAILING_ID = Identifier.of(MOD_ID, "test_railing");
+	public static final HorizontalBorderBlock TEST_BLOCK = new HorizontalBorderBlock(AbstractBlock.Settings.create()
 			.mapColor(MapColor.STONE_GRAY)
+			.sounds(BlockSoundGroup.METAL)
 			.instrument(NoteBlockInstrument.BASEDRUM)
 			.requiresTool().strength(3.5f));
 	public static final BlockItem TEST_ITEM = new BlockItem(TEST_BLOCK, new Item.Settings());
 
 	@Override
 	public void onInitialize() {
-		Registry.register(Registries.BLOCK, RAILINGS_ID, TEST_BLOCK);
-		Registry.register(Registries.ITEM, RAILINGS_ID, TEST_ITEM);
+		Registry.register(Registries.BLOCK, TEST_RAILING_ID, TEST_BLOCK);
+		Registry.register(Registries.ITEM, TEST_RAILING_ID, TEST_ITEM);
 		LOGGER.info(MOD_ID + " | Loaded Successfully");
 	}
 }
--- mavila-railings/src/main/java/mod/ginleaf/railings/block/HorizontalBorderBlock.java
+++ mavila-railings/src/main/java/mod/ginleaf/railings/block/HorizontalBorderBlock.java
@@ -4,77 +4,109 @@ import com.mojang.serialization.MapCodec;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.Waterloggable;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.ai.pathing.NavigationType;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.fluid.FluidState;
+import net.minecraft.fluid.Fluids;
 import net.minecraft.item.*;
 import net.minecraft.state.StateManager;
 import net.minecraft.state.property.BooleanProperty;
-import net.minecraft.state.property.IntProperty;
 import net.minecraft.state.property.Properties;
 import net.minecraft.util.Hand;
 import net.minecraft.util.ItemActionResult;
 import net.minecraft.util.ItemScatterer;
 import net.minecraft.util.hit.BlockHitResult;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.BlockView;
 import net.minecraft.world.World;
 import net.minecraft.world.WorldAccess;
 import org.jetbrains.annotations.Nullable;
 
+import java.util.Objects;
+
 public class HorizontalBorderBlock extends Block implements Waterloggable {
     public static final MapCodec<HorizontalBorderBlock> CODEC = HorizontalBorderBlock.createCodec(HorizontalBorderBlock::new);
     public static final BooleanProperty WATERLOGGED = Properties.WATERLOGGED;
-    public static final IntProperty COMBINATIONS = IntProperty.of("combinations", 1, 15);
     public static final BooleanProperty NORTH = BooleanProperty.of("north");
     public static final BooleanProperty WEST = BooleanProperty.of("west");
     public static final BooleanProperty SOUTH = BooleanProperty.of("south");
     public static final BooleanProperty EAST = BooleanProperty.of("east");
-    private int borderCount;
+    public static final BooleanProperty[] FACE_PROPERTIES = {NORTH, WEST, SOUTH, EAST};
 
     public HorizontalBorderBlock(Settings settings) {
         super(settings);
-        this.borderCount = 1;
         this.setDefaultState(this.stateManager.getDefaultState()
                 .with(WATERLOGGED, false)
                 .with(NORTH, false)
                 .with(WEST, false)
                 .with(SOUTH, false)
-                .with(EAST, false)
-        );
+                .with(EAST, false));
     }
 
-    @Nullable
-    public BlockState getPlacementState(ItemPlacementContext ctx) {
-        World world = ctx.getWorld();
-        BlockPos blockPos = ctx.getBlockPos();
-        BlockState blockState = world.getBlockState(blockPos);
-        world.setBlockState(blockPos, blockState.with(COMBINATIONS,1 << (ctx.getHorizontalPlayerFacing().getHorizontal() / 90 - 1)));
-        return blockState;
-    }
-
-    @Override
-    protected ItemActionResult onUseWithItem(ItemStack stack, BlockState state, World world, BlockPos pos, PlayerEntity player, Hand hand, BlockHitResult hit) {
-        if(stack.getItem() instanceof BlockItem blockItem) {
-            if(this.borderCount >= 4) return ItemActionResult.FAIL;
-            if(!(blockItem.getBlock().getName().equals(this.getName()))) return ItemActionResult.FAIL;
-            int combinations = state.get(COMBINATIONS);
-            int directionBit = 1 << (player.getHorizontalFacing().getHorizontal() / 90 - 1);
-            if((combinations | directionBit) == combinations) return ItemActionResult.FAIL;
-            stack.decrement(1);
-            this.borderCount++;
-            world.setBlockState(pos, state.with(COMBINATIONS, combinations | directionBit));
-            return ItemActionResult.CONSUME;
-        }
-        return ItemActionResult.FAIL;
-    }
-
-    @Override
-    public void onBroken(WorldAccess world, BlockPos pos, BlockState state) {
-        ItemStack itemStack = new ItemStack(world.getBlockState(pos).getBlock().asItem());
-        itemStack.setCount(this.borderCount);
-        ItemScatterer.spawn((World) world,pos.getX(),pos.getY(),pos.getZ(),itemStack);
+    protected void incrementState(World world, BlockPos blockPos, BlockState blockState, BooleanProperty property) {
+        world.setBlockState(blockPos, blockState.with(property, true));
     }
 
     @Override
     protected void appendProperties(StateManager.Builder<Block, BlockState> builder) {
         builder.add(WATERLOGGED, NORTH, WEST, SOUTH, EAST);
+    }
+
+    @Nullable
+    @Override
+    public BlockState getPlacementState(ItemPlacementContext ctx) {
+        FluidState fluidState = ctx.getWorld().getFluidState(ctx.getBlockPos());
+        return Objects.requireNonNull(super.getPlacementState(ctx)).with(WATERLOGGED, fluidState.getFluid() == Fluids.WATER);
+    }
+
+    @Override
+    protected boolean isTransparent(BlockState state, BlockView world, BlockPos pos) {
+        return true;
+    }
+
+    @Override
+    public void onPlaced(World world, BlockPos pos, BlockState state, @Nullable LivingEntity placer, ItemStack itemStack) {
+        int dir = (placer != null) ? placer.getHorizontalFacing().getHorizontal() : 0;
+        incrementState(world,pos,state, FACE_PROPERTIES[dir]);
+    }
+
+    @Override
+    protected ItemActionResult onUseWithItem(ItemStack stack, BlockState state, World world, BlockPos pos, PlayerEntity player, Hand hand, BlockHitResult hit) {
+        if(stack.getItem() instanceof BlockItem blockItem) {
+            if(!(blockItem.getBlock().getName().equals(this.getName()))) return ItemActionResult.PASS_TO_DEFAULT_BLOCK_INTERACTION;
+            int dir = player.getHorizontalFacing().getHorizontal();
+            int bitMask = 0;
+            for(int i = 0; i < 4; i++) {
+                bitMask += (state.get(FACE_PROPERTIES[i]) ? 1 << i : 0);
+            }
+            if((bitMask | (1 << dir)) == bitMask) return ItemActionResult.PASS_TO_DEFAULT_BLOCK_INTERACTION;
+            stack.decrementUnlessCreative(1,player);
+            incrementState(world,pos,state, FACE_PROPERTIES[dir]);
+            return ItemActionResult.SUCCESS;
+        } else {
+            return ItemActionResult.PASS_TO_DEFAULT_BLOCK_INTERACTION;
+        }
+    }
+
+    @Override
+    public void onBroken(WorldAccess world, BlockPos pos, BlockState state) {
+        int returnAmount = 0;
+        for(int i = 0; i < 4; i++) {
+            returnAmount += state.get(FACE_PROPERTIES[i]) ? 1 : 0;
+        }
+        ItemStack itemStack = new ItemStack(state.getBlock().asItem());
+        itemStack.setCount(returnAmount);
+        ItemScatterer.spawn((World) world,pos.getX(),pos.getY(),pos.getZ(),itemStack);
+    }
+
+    @Override
+    protected FluidState getFluidState(BlockState state) {
+        return state.get(WATERLOGGED) ? Fluids.WATER.getStill(false) : super.getFluidState(state);
+    }
+
+    @Override
+    protected boolean canPathfindThrough(BlockState state, NavigationType type) {
+        return false;
     }
 }
